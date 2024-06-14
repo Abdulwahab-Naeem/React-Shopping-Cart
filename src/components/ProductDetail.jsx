9a63a08131de666fdd4f154647a954eac4d90bae
Abdulwahab-Naeem <Abdulwahabnaeem780@gmail.com>
2024-06-14 17:08:47 +0500

    React-Shopping-Cart-App

---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { items } from './Data'
+import Product from './Product'
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const ProductDetail = ({cart,setCart}) => {
+    const {id}=useParams()
+    const[product,setProduct]=useState({})
+    const [relatedProducts, setrelatedProducts] = useState([])
+    useEffect(()=>{
+     const filterProduct=items.filter((product)=>product.id==id)
+    //  console.log(filterProduct);
+    setProduct(filterProduct[0])
+    const relatedProducts=items.filter((p)=>p.category===product.category)
+    // console.log("Realted Products",relatedProducts);
+    setrelatedProducts(relatedProducts);
+    
+},[id,product.category]);
+const addToCart = (id, price, title, description, imgSrc) => {
+  const obj = {
+    id, price, title, description, imgSrc
+  };
+  setCart([...cart, obj]);
+  toast.success(`${title} added to cart!`);
+  console.log("Cart element =", cart);
+};
+
+  return (
+   <>
+   <ToastContainer
+        position="top-right"
+        autoClose={15000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
+   <div className="container con">
+    <div className="img">
+     <img src={product.imgSrc}/>
+    </div>
+    <div className='text-center'>
+    <h1 className="card-title">{product.title}</h1>
+                    <p className="card-text">{product.description}</p>
+                    <button className='btn btn-primary mx-3'>{product.price}{" "}₹</button>
+                    <button
+                        onClick={() => addToCart(
+                          product.id,
+                          product.price,
+                          product.title,
+                          product.description,
+                          product.imgSrc
+                        )}
+                        className='btn btn-warning'>Add To Cart</button>
+    </div>
+   </div>
+   <h1 className='text-center'>Related Products</h1>
+   <Product cart={cart} setCart={setCart} items={relatedProducts} />
+   </>
+  )
+}
+
+export default ProductDetail
